5ce9c283edd7d60dcd0ec28d6091325b645f5797
Honorine Marie Christy <honorine@24eme.fr>
2020-06-02 16:15:27 +0200

    les js

--- view/js/script.js
+++ view/js/script.js
@@ -1,12 +1,30 @@
-function Generate{
+function Generate(){
 
-  var rand = ;
+  // var tab=[];
+  var res = document.getElementById('resultat');
+  var textarea = document.getElementById('textarea');
 
-  if (.match()){
 
-  }
-  else {
+  var data= (textarea.value).toString();  // recupere le texte dans le texte area
+  const tab = data.split('\n'); 
 
-  }
+  var max = tab.length-1;
+  var min = 0;
+  var rand = Math.floor(Math.random()*(max-min +1))+min ;
+
+  res.innerText=tab[rand];
+
+  //console.log(res.innerText);
+  //console.log(rand);
+  // }
+
+
+
+}
+function Reinitialiser(){
+  var res = document.getElementById('resultat');
+  var textarea = document.getElementById('textarea');
+  textarea.value="";
+
 
 }
